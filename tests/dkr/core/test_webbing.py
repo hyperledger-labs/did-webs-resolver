73ffe38ba932d28b256a536814e47e280de4d928
2byrds <2byrds@gmail.com>
2024-01-08 20:05:47 -0500

    prepping for webbing/service testing
    Signed-off-by: 2byrds <2byrds@gmail.com>

---
+++ tests/dkr/core/test_webbing.py
@@ -0,0 +1,148 @@
+import aiohttp
+import asyncio
+
+import json
+
+import falcon
+from falcon import testing, media, http_status
+from hio.base import doing
+
+from dkr.core import webbing
+
+from hio.base import tyming
+from hio.core import http
+
+from keri import help, kering
+from keri.app import configing, habbing, oobiing
+from keri.core import coring
+from keri.db import basing
+from keri.end import ending
+from keri.help import helping
+
+import os
+import queue
+import threading
+import time
+        
+class PingResource:
+   def on_get(self, req, resp):
+      """Handles GET requests"""
+      resp.status = falcon.HTTP_200
+      resp.content_type = falcon.MEDIA_TEXT
+      resp.text = (
+         'Pong'
+      )
+
+def test_service():
+    with habbing.openHby(name="service") as hby:
+        hab = hby.makeHab(name="service")
+        aid = "ELCUOZXs-0xn3jOihm0AJ-L8XTFVT8SnIpmEDhFF9Kz_"
+        did = f"did:web:127.0.0.1%3a7676:{aid}"
+        cf = configing.Configer(name="config-test",
+                        base=None,
+                        headDirPath="volume/dkr/examples/my-scripts",
+                        temp=True,
+                        reopen=False,
+                        clear=True)
+
+        # Configure the did doc and keri cesr URL
+        ddurl = f'http://127.0.0.1:7676/{aid}/did.json'
+        kcurl = f'http://127.0.0.1:7676/{aid}/keri.cesr'
+        purl = "http://127.0.0.1:7676/ping"
+        puburl = "http://example.org"
+
+        app = falcon.App(middleware=falcon.CORSMiddleware(
+        allow_origins='*', allow_credentials='*',
+        expose_headers=['cesr-attachment', 'cesr-date', 'content-type', 'signature', 'signature-input',
+                        'signify-resource', 'signify-timestamp']))
+
+        print("CORS  enabled")
+        app.add_middleware(middleware=HandleCORS())
+        app.req_options.media_handlers.update(media.Handlers())
+        app.resp_options.media_handlers.update(media.Handlers())
+        # falcon.App instances are callable WSGI apps
+
+        app.add_route('/ping', PingResource())
+        service = webbing.setup(app, hby, cf)
+        # app.add_route('/{aid}/did.json', DidWebsEnd())
+        # app.add_route('/{aid}/keri.cesr', KeriCesrEnd(aid=aid))
+
+        server = http.Server(host="127.0.0.1",port=7611, app=app, scheme="http")
+        httpServerDoer = http.ServerDoer(server=server)
+
+        # client = testing.TestClient(app=app)        
+        # rep = client.simulate_get('/example')
+        # assert rep.status == falcon.HTTP_OK
+        # assert rep.text == '\nHello World\n\n'
+
+        limit = 2.0
+        tock = 0.03125
+        doers = [httpServerDoer]
+        # doers = [httpServerDoer] + resDoer.doers
+        doist = doing.Doist(limit=limit, tock=tock, doers=doers)
+        # doist.do(doers=doers)
+
+        doist.enter()
+        tymer = tyming.Tymer(tymth=doist.tymen(), duration=doist.limit)
+
+        estat = None
+        etres = queue.Queue()
+        et = threading.Thread(target=resDoer.loadUrl, args=(eurl,etres))
+        et.start()
+        
+        ddstat = None
+        ddtres = queue.Queue()
+        ddt = threading.Thread(target=resDoer.loadUrl, args=(ddurl,ddtres))
+        ddt.start()
+        
+        kcstat = None
+        kctres = queue.Queue()
+        kct = threading.Thread(target=resDoer.loadUrl, args=(kcurl,kctres))
+        kct.start()
+        
+        while estat == None or ddtres == None:
+            # resp = resDoer.loadUrl(ddurl)
+            # status = asyncio.run(call(eurl))
+            # resDoer.loadUrl(kcurl)
+            # resDoer.resolve(tymth=doist.tymen(), tock=doist.tock)
+            time.sleep(2)
+            doist.recur()
+
+            resp = etres.get()
+            estat = resp.status_code
+            assert estat == 200
+            print("Got example response content", resp.content)
+            
+            resp = ddtres.get()
+            ddstat = resp.status_code
+            assert ddstat == 200
+            print("Got dd response content", resp.content)
+            
+            resp = kctres.get()
+            kcstat = resp.status_code
+            assert kcstat == 200
+            print("Got kc response content", resp.content)
+        doist.exit()
+
+        
+
+        """Done Test"""
+    
+class HandleCORS(object):
+    def process_request(self, req, resp):
+        resp.set_header('Access-Control-Allow-Origin', '*')
+        resp.set_header('Access-Control-Allow-Methods', '*')
+        resp.set_header('Access-Control-Allow-Headers', '*')
+        resp.set_header('Access-Control-Max-Age', 1728000)  # 20 days
+        if req.method == 'OPTIONS':
+            raise http_status.HTTPStatus(falcon.HTTP_200, text='\n')
+        
+# async def fetch(session, url):
+#     async with session.get(url) as response:
+#         return response.status
+
+# async def call(url):
+#     async with aiohttp.ClientSession() as session:
+#         status = await fetch(session, url)
+#         print(status)
+#         return status
